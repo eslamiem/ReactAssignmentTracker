4a53a0819fdc49329b0ff79488f201f94dd72564
Marzieh Eslami <eslami.marzieh@gmail.com>
2025-10-19 17:46:11 -0700

    clean code

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -30,7 +30,6 @@ export function Header({ setAssignments }: Props) {
 
   return (
     <header className={styles.header}>
-      {/* This is simply to show you how to use helper functions */}
       <h1>{uppercase("bcit")} Assignment Tracker</h1>
       <form className={styles.newAssignmentForm} onSubmit={(e) => handleCreate(e)}>
         <input value={text} onChange={(e) => setText(e.target.value)} placeholder="Add a new assignment" type="text" />
